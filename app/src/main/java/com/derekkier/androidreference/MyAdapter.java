de41e35e23c3b3c69ac3898abfbbf8c4f67da275
derekkier <derek.kier@gmail.com>
2015-09-09 23:28:27 -0700

    Stage files properly for commit

--- app/src/main/java/com/derekkier/androidreference/MyAdapter.java
+++ app/src/main/java/com/derekkier/androidreference/MyAdapter.java
@@ -1,7 +1,38 @@
 package com.derekkier.androidreference;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
- * Created by Toshiba on 9/9/2015.
+ * Created by Derek Kier on 9/9/2015.
  */
-public class MyAdapter {
+public class MyAdapter extends ArrayAdapter<String> {
+
+    public MyAdapter(Context context, String[] values) {
+        super(context, R.layout.row_layout_2,values);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        LayoutInflater theInflator = LayoutInflater.from(getContext());
+
+        View theView = theInflator.inflate(R.layout.row_layout_2, parent, false);
+
+        String tvShow = getItem(position);
+
+        TextView theTextView = (TextView) theView.findViewById(R.id.textView1);
+        theTextView.setText(tvShow);
+        theTextView.setTextColor(Color.parseColor("#ff8800"));
+
+        ImageView theImageView = (ImageView) theView.findViewById(R.id.imageView1);
+        theImageView.setImageResource(R.drawable.shaded_dot);
+        return theView;
+    }
 }
