1e8e98b4d348e275d2263fe9d2f10decc9b33da1
derekkier <derek.kier@gmail.com>
2015-10-08 16:11:38 -0700

    Test that it can get JSON from URL.
    This example works to make the connection and retrieve the JSON string.

--- app/src/main/java/com/derekkier/androidreference/MainActivity.java
+++ app/src/main/java/com/derekkier/androidreference/MainActivity.java
@@ -1,200 +1,107 @@
 package com.derekkier.androidreference;
 
-import android.app.Activity;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import android.app.Activity;
 
 public class MainActivity extends Activity {
 
-    // The JSON REST Service I will pull from
-    static String yahooStockInfo = "http://query.yahooapis.com/v1/public/yql?" + "q=select%20*%20from%20yahoo.finance.quote%20where%20symbol" + "%20in%20(%22YHOO%22)&format=json&env=store%3A%2F%2" + "Fdatatables.org%2Falltableswithkeys&callback=cbfunc";
-
-    // Will hold the values I pull from the JSON
-    static String stockSymbol = "";
-    static String stockDaysLow = "";
-    static String stockDaysHigh = "";
-    static String stockChange = "";
-
+    EditText etResponse;
+    TextView tvIsConnected;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // Get any saved data
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Point to the name for the layout xml file used
         setContentView(R.layout.activity_main);
 
-        // Call for doInBackground() in MyAsyncTask to be executed
-        new MyAsyncTask().execute();
+        // get reference to the views
+        etResponse = (EditText) findViewById(R.id.etResponse);
+        tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
 
+        // check if you are connected or not
+        if(isConnected()){
+            tvIsConnected.setBackgroundColor(0xFF00CC00);
+            tvIsConnected.setText("You are conncted");
+        }
+        else{
+            tvIsConnected.setText("You are NOT conncted");
+        }
+
+        // call AsynTask to perform network operation on separate thread
+        new HttpAsyncTask().execute("http://hmkcode.appspot.com/rest/controller/get.json");
     }
-    // Use AsyncTask if you need to perform background tasks, but also need
-    // to change components on the GUI. Put the background operations in
-    // doInBackground. Put the GUI manipulation code in onPostExecute
 
-    private class MyAsyncTask extends AsyncTask<String, String, String> {
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
 
-        protected String doInBackground(String... arg0) {
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
 
-            // HTTP Client that supports streaming uploads and downloads
-            DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
 
-            // Define that I want to use the POST method to grab data from
-            // the provided URL
-            HttpPost httppost = new HttpPost(yahooStockInfo);
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
 
-            // Web service used is defined
-            httppost.setHeader("Content-type", "application/json");
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
 
-            // Used to read data from the URL
-            InputStream inputStream = null;
-
-            // Will hold the whole all the data gathered from the URL
-            String result = null;
-
-            try {
-
-                // Get a response if any from the web service
-                HttpResponse response = httpclient.execute(httppost);
-
-                // The content from the requested URL along with headers, etc.
-                HttpEntity entity = response.getEntity();
-
-                // Get the main content from the URL
-                inputStream = entity.getContent();
-
-                // JSON is UTF-8 by default
-                // BufferedReader reads data from the InputStream until the Buffer is full
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
-
-                // Will store the data
-                StringBuilder theStringBuilder = new StringBuilder();
-
-                String line = null;
-
-                // Read in the data from the Buffer untilnothing is left
-                while ((line = reader.readLine()) != null)
-                {
-
-                    // Add data from the buffer to the StringBuilder
-                    theStringBuilder.append(line + "\n");
-                }
-
-                // Store the complete data in result
-                result = theStringBuilder.toString();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-
-                // Close the InputStream when you're done with it
-                try{if(inputStream != null)inputStream.close();}
-                catch(Exception e){}
-            }
-
-            // Holds Key Value pairs from a JSON source
-            JSONObject jsonObject;
-            try {
-
-                // Delete cbfunc( and ); from the results
-                result = result.substring(7);
-                result = result.substring(0, result.length()-2);
-
-                // Print out all the data read in
-                // Log.v("JSONParser RESULT ", result);
-
-                // Get the root JSONObject
-                jsonObject = new JSONObject(result);
-
-                // Get the JSON object named query
-                JSONObject queryJSONObject = jsonObject.getJSONObject("query");
-
-                // Get the JSON object named results inside of the query object
-                JSONObject resultsJSONObject = queryJSONObject.getJSONObject("results");
-
-                // Get the JSON object named quote inside of the results object
-                JSONObject quoteJSONObject = resultsJSONObject.getJSONObject("quote");
-
-                // Get the JSON Strings in the quote object
-                stockSymbol = quoteJSONObject.getString("symbol");
-                stockDaysLow = quoteJSONObject.getString("DaysLow");
-                stockDaysHigh = quoteJSONObject.getString("DaysHigh");
-                stockChange = quoteJSONObject.getString("Change");
-
-                // EXTRA STUFF THAT HAS NOTHING TO DO WITH THE PROGRAM
-
-                Log.v("SYMBOL ", stockSymbol);
-                Log.v("Days Low ", stockDaysLow);
-                Log.v("Days High ", stockDaysHigh);
-                Log.v("Change ", stockChange);
-
-                // GET ARRAY DATA
-                JSONArray queryArray = quoteJSONObject.names();
-
-                List<String> list = new ArrayList<String>();
-                for (int i=0; i<queryArray.length(); i++) {
-                    list.add( queryArray.getString(i) );
-                }
-
-                for(String item : list){
-
-                    Log.v("JSON ARRAY ITEMS ", item);
-
-                }
-                // END OF GET ARRAY DATA
-
-                // Gets the first item in the JSONObject
-                JSONArray objectArray = resultsJSONObject.names();
-
-                // Prints out that first item in the JSONObject
-                Log.v("JSON NEXT NODE ", objectArray.getString(0));
-
-
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-            return result;
-
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
         }
 
-        protected void onPostExecute(String result){
+        return result;
+    }
 
-            // Gain access so I can change the TextViews
-            TextView line1 = (TextView)findViewById(R.id.line1);
-            TextView line2 = (TextView)findViewById(R.id.line2);
-            TextView line3 = (TextView)findViewById(R.id.line3);
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
 
-            // Change the values for all the TextViews
-            line1.setText("Stock: " + stockSymbol + " : " + stockChange);
-
-            line2.setText("Days Low: " + stockDaysLow);
-
-            line3.setText("Days High: " + stockDaysHigh);
-
-        }
+        inputStream.close();
+        return result;
 
     }
 
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            etResponse.setText(result);
+        }
+    }
 }
