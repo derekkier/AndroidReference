c41d3cad8ddd358b771513c80bdc257077745968
derekkier <derek.kier@gmail.com>
2015-10-12 14:08:28 -0700

    Commit work made so far

--- app/src/main/java/com/derekkier/androidreference/MainActivity.java
+++ app/src/main/java/com/derekkier/androidreference/MainActivity.java
@@ -1,40 +1,73 @@
 package com.derekkier.androidreference;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
 
-    // private ProgressDialog dialog;
+public class MainActivity extends Activity implements
+    ConnectionCallbacks, OnConnectionFailedListener {
 
-    public void onCreate(Bundle savedInstanceState) {
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    TextView tvLatlong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "onCreate", Toast.LENGTH_LONG).show();
+
+        tvLatlong = (TextView) findViewById(R.id.tvLatlong);
+
+        buildGoogleApiClient();
+
+        if(mGoogleApiClient!= null){
+            mGoogleApiClient.connect();
+        }
+        else
+            Toast.makeText(this, "Not connected...", Toast.LENGTH_SHORT).show();
+
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onConnectionFailed(ConnectionResult arg0) {
+        Toast.makeText(this, "Failed to connect...", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onConnected(Bundle arg0) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            tvLatlong.setText("Latitude: "+ String.valueOf(mLastLocation.getLatitude())+"Longitude: "+
+                    String.valueOf(mLastLocation.getLongitude()));
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        Toast.makeText(this, "Connection suspended...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build();
     }
 }
